337dd554d107f0244af206209a7adc4d1f8b1e80
hilal-najem <hilal123work@gmail.com>
2023-04-25 13:35:34 +0300

    feat: Setup AddressHelper

---
+++ app/Containers/Addresses/Helpers/AddressHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Containers\Addresses\Helpers;
+
+use App\Containers\Addresses\Model\Address;
+
+use App\Helpers\BaseHelper;
+
+class AddressHelper extends BaseHelper
+{
+    protected static string $messageKeyBase = 'ADDRESS';
+    protected static string $modelName = 'Address';
+    protected static string $model = Address::class;
+    protected static $allowed = ['id', 'all', 'create', 'update'];
+
+    protected static function model()
+    {
+        return self::$model;
+    }
+
+    protected static function message()
+    {
+        return self::$messageKeyBase;
+    }
+
+    protected static function allowed()
+    {
+        return self::$allowed;
+    }
+}
--- app/Containers/Addresses/Messages/messages.php
+++ app/Containers/Addresses/Messages/messages.php
@@ -7,6 +7,10 @@ class Messages
     public static function messages()
     {
         return [
+            'ADDRESS' => [
+                'NAME' => 'Address',
+                'NOT_FOUND' => 'Address not found'
+            ]
         ];
     }
 }
